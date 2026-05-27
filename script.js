83ae8862d1bbf44f22898296feb500c0668ed7c8
Yanfth <alfianfatoni1405@gmail.com>
2026-05-27 16:27:19 +0700

    Add JS for Order Section

---
+++ script.js
@@ -0,0 +1,14 @@
+const pilihanMenu = document.getElementById('pilihan-menu');
+const jumlahPorsi = document.getElementById('jumlah-porsi');
+const totalHargaDisplay = document.getElementById('total-harga');
+
+function hitungTotal() {
+  const harga = parseInt(pilihanMenu.value);
+  const porsi = parseInt(jumlahPorsi.value) || 0;
+  const total = harga * porsi;
+
+  totalHargaDisplay.innerText = "Rp " + total.toLocaleString('id-ID');
+}
+
+pilihanMenu.addEventListener('change', hitungTotal);
+jumlahPorsi.addEventListener('input', hitungTotal);
